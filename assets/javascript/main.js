8d1d15358482aa51ed89eb96406ba359bce2e8f5
RamonJul <ramoncarlosjulian@gmail.com>
2018-12-30 13:10:12 -0500

    Basic Foundation and Minimum Requirments

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,7 +3,7 @@ var destination_box = document.getElementById("destination")
 var frequency_box = document.getElementById("frequency")
 var arrival_box = document.getElementById("next_arrival")
 var time_left_box = document.getElementById("time_left")
-
+var number=0;
 
 // Initialize Firebase
 var config = {
@@ -17,7 +17,7 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-
+// console.log(database.ref().val())
 
 
 
@@ -27,15 +27,15 @@ document.getElementById("submit").addEventListener("click", function () {
     var frequency = frequency_box.value;
     var arrival = arrival_box.value;
     var time = time_left_box.value;
-
-    database.ref("/train_info").push({
+    database.ref().push({
         train: train,
         destination: destination,
         frequency: frequency,
         arrival: arrival,
-        time: time
+        time: time,
+        train_number:number
     })
-
+    number++
     train_box.value = "";
     destination_box.value = "";
     frequency_box.value = "";
@@ -45,8 +45,17 @@ document.getElementById("submit").addEventListener("click", function () {
 
 
 })
+    
+database.ref().on("value", function (snapshopt) {
+    console.log(snapshopt.val())
+    snapshopt.forEach((child) => {
+        console.log(child.key, child.val()); 
 
-database.ref("/train_info").on("child_added", function (snapshopt) {
+      });
+
+})
+database.ref().on("child_added", function (snapshopt) {
+    console.log(snapshopt)
     console.log(snapshopt.val())
 
     var row = document.createElement("tr");
@@ -77,31 +86,31 @@ database.ref("/train_info").on("child_added", function (snapshopt) {
 
 
 
-// AUTHENTICATION
-    var auth = firebase.auth()
+// // AUTHENTICATION
+//     var auth = firebase.auth()
 
-    var email_box=document.getElementById("email")
-    var password_box=document.getElementById("password")
+//     var email_box=document.getElementById("email")
+//     var password_box=document.getElementById("password")
 
-    document.getElementById("SignuP").addEventListener("click", function(){
-        //check for valid email
-        var email=email_box.value
-        var password=password_box.value
+//     document.getElementById("SignuP").addEventListener("click", function(){
+//         //check for valid email
+//         var email=email_box.value
+//         var password=password_box.value
 
-        var user=auth.createUserWithEmailAndPassword(email,password)
+//         var user=auth.createUserWithEmailAndPassword(email,password)
 
 
-    })
+//     })
 
-    firebase.auth().onAuthStateChanged(firebaseUser =>{
+//     firebase.auth().onAuthStateChanged(firebaseUser =>{
 
-        if(firebaseUser){
-            console.log(firebaseUser);
-        } else{
-            console.log("not logged in")
-        }
+//         if(firebaseUser){
+//             console.log(firebaseUser);
+//         } else{
+//             console.log("not logged in")
+//         }
 
     
 
 
-    })
+    // })
