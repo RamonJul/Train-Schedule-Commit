c23e546519a1bff86fa460619225de58e8663e05
RamonJul <ramoncarlosjulian@gmail.com>
2019-01-08 16:07:58 -0500

    finalize to test login

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,7 +3,7 @@ var destination_box = document.getElementById("destination")
 var frequency_box = document.getElementById("frequency")
 var arrival_box = document.getElementById("next_arrival")
 var intervalId;
-var internal_time=0
+var internal_time = 0
 
 // Initialize Firebase
 var config = {
@@ -16,8 +16,20 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+var now = moment()//initial time when could runs
+var now_time = moment(now.hour() + ":" + now.minutes(), "HH:mm", true)
+console.log(now_time)
+var now = moment()//initial time when could runs
+console.log(now)
+if(now.minutes()<10){
+    var now_time = moment(now.hour() + ":0" +now.minutes(), "HH:mm", true)
+}
+else{
+var now_time = moment(now.hour() + ":" + now.minutes(), "HH:mm", true)
+}
+document.getElementById("current_time").textContent=now_time.format("HH:mm")
 
-// console.log(database.ref().val())
+
 
 
 // add new data
@@ -25,81 +37,136 @@ document.getElementById("submit").addEventListener("click", function () {
     var train = train_box.value;
     var destination = destination_box.value;
     var frequency = frequency_box.value;
-    var arrival =arrival_box.value
+    var arrival = arrival_box.value
     database.ref("/train").push({
         train: train,
         destination: destination,
         frequency: frequency,
         arrival: arrival,
-        eta:frequency
-    
+
     })
 
     train_box.value = "Trian";
     destination_box.value = "Destinatoin";
     frequency_box.value = "Frequency";
     arrival_box.value = "First Train";
-
+   
 
 
 })
-    
 
-function myFunction() {// test new stuff
-    var arrival=document.getElementsByClassName("arrival")
-
+//TEST FUNCTION
+function myFunction() { // test new stuff
+    var time= moment("16:03","HH:mm", true)
+    console.log(time)
+    console.log(time.format("HH:mm"))
+    if(time.minutes()<10){
+        var test_time = moment(time.hour() + ":0" +time.minutes(), "HH:mm", true)
+    }
+    else{
+    var test_time = moment(time.hour() + ":" + time.minutes(), "HH:mm", true)
+    }
+    console.log(test_time.format("HH:mm"))
   
-    for(var i=0; i<arrival.length;i++){
-        console.log(arrival)
-        var key= arrival[i].parentElement.getAttribute("key");
-        var time_left=arrival[i].textContent
-       var eta= parseInt(time_left);
-       eta--
-     
-        if(eta>0){
-            arrival[i].textContent=eta
-           console.log("not done")
-           database.ref("/train/"+key).update({
-            eta:eta
-        })
-             
-        }
-
-        else{//train arrives
-            console.log("arrived")
-           
-            var frequency=arrival[i].previousSibling.previousSibling.textContent
-            var old_arrival_=arrival[i].previousSibling.textContent
-            var update=moment(old_arrival_,"hh:mm")
-            var min_update=update.minute()+parseInt(frequency)
-            update.set("minute", min_update)//updated time value
-            var new_time=update.format("hh:mm")
-            arrival[i].textContent=frequency
-            arrival[i].previousSibling.textContent=new_time
-            database.ref("/train/"+key).update({
-                
-                arrival:new_time,
-                eta:frequency
-            })
-            //moment stuff    
-
-        }
+    // var provider = new firebase.auth.GoogleAuthProvider()
+    // firebase.auth().signInWithPopup(provider).then(function (result) {
+    //     // This gives you a Google Access Token. You can use it to access the Google API.
+    //     var token = result.credential.accessToken;
+    //     console.log("success")
+    //     // The signed-in user info.
+    //     var user = result.user;
+    //     // ...
+    // }).catch(function (error) {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // The email of the user's account used.
+    //     var email = error.email;
+    //     // The firebase.auth.AuthCredential type that was used.
+    //     var credential = error.credential;
+    //     // ...
+    // });
+    
 
 
 }
-  
+
 //checks is database has been running
-  database.ref("/time").on("value", function(snapshot){
-    if(snapshot.child("system_time").exists()){
-        internal_time=snapshot.val().system_time
+database.ref("/time").on("value", function (snapshot) {
+    if (snapshot.child("system_time").exists()) {
+        internal_time = snapshot.val().system_time
 
     }
 
+    
 })
 // appends data on to html
+
 database.ref("/train").on("child_added", function (snapshot) {
+   
+    display(snapshot)
+})
+
+database.ref("/train").on("child_changed", function (snapshot) {
+    var key =snapshot.key
+    document.getElementById("list").removeChild(document.getElementById(key))
+    display(snapshot)
+})
+
+
+function table_headers() {
     var row = document.createElement("tr");
-    row.setAttribute("key", snapshot.key)
+    var empty = document.createElement("th");
+    row.appendChild(empty)
+
+    var train = document.createElement("th");
+    train.setAttribute("scope", "col")
+    train.textContent = "Train"
+    row.appendChild(train)
+
+    var destination = document.createElement("th");
+    destination.setAttribute("scope", "col")
+    destination.textContent = "Destination"
+    row.appendChild(destination)
+
+    var frequency = document.createElement("th");
+    frequency.setAttribute("scope", "col")
+    frequency.textContent = "Frequency"
+    row.appendChild(frequency)
+
+    var time = document.createElement("th");
+    time.setAttribute("scope", "col")
+    time.textContent = "Time"
+    row.appendChild(time)
+
+    var ETA = document.createElement("th");
+    ETA.setAttribute("scope", "col")
+    ETA.textContent = "ETA"
+    row.appendChild(ETA)
+
+    var empty_2 = document.createElement("th");
+    row.appendChild(empty_2)
+
+    document.getElementById("list").appendChild(row);
+
+
+}
+
+function display(snapshot) {
+  
+    // displaying the data into the array
+    var row = document.createElement("tr");
+    row.setAttribute("id", snapshot.key)
+
+    var buttons = document.createElement("td");
+    var update = document.createElement("button")
+    update.textContent = "Update"
+    update.setAttribute("data-toggle", "modal")
+    update.setAttribute("data-target", "#update_box")
+    update.setAttribute("class", "btn btn-primary update")
+    update.addEventListener("click", update_data)
+    buttons.appendChild(update)
+    row.appendChild(buttons)
 
     var train_data = document.createElement("td");
     train_data.textContent = snapshot.val().train;
@@ -113,56 +180,77 @@ database.ref("/train").on("child_added", function (snapshot) {
     frequency.textContent = snapshot.val().frequency;
     row.appendChild(frequency);
 
-    var time= document.createElement("td");
+    var time = document.createElement("td");
     time.textContent = snapshot.val().arrival;
     row.appendChild(time);
 
+    var calc_time = moment(snapshot.val().arrival, "HH:mm", true)
+    var estimated_time = calc_time.diff(now_time, "minutes")
+
+    if (estimated_time < 0) {
+        estimated_time += 1440
+    }
     var eta = document.createElement("td");
-    eta.textContent = snapshot.val().eta;
+    eta.textContent = estimated_time;
     eta.classList.add("arrival")
     row.appendChild(eta);
 
 
+
+    var remove = document.createElement("button")
+    remove.setAttribute("class", "remove")
+    remove.setAttribute("class", "btn btn-primary")
+    remove.textContent = "X"
+    remove.addEventListener("click", removing)
+    var button_2= document.createElement("td");
+    button_2.appendChild(remove)
+    row.appendChild(button_2);
+
     document.getElementById("list").appendChild(row);
 
-        
-})
-// internal timer
+
+}
+// internal timer update the time
 
 intervalId=setInterval(function(){
     internal_time+=1000;
     database.ref("/time").set({
     system_time: internal_time
-    
+
     })
-    console.log(internal_time)
-    if(internal_time%60000===0){
-     //1 min
+    // console.log(internal_time)
+    if(internal_time%1000===0){
+    // every second
+    var current_now=moment()
+    if(current_now.minutes()<10){
+        var  current_now_time= moment(current_now.hour() + ":0" +current_now.minutes(), "HH:mm", true)
+    }
+    else{
+        var current_now_time=moment(current_now.hour() + ":" + current_now.minutes(), "HH:mm", true)
+    }
+    document.getElementById("current_time").textContent=current_now_time.format("HH:mm")
+    console.log(Math.abs(current_now_time.diff(now_time,"minutes")))
     
+    if(Math.abs(current_now_time.diff(now_time,"minutes"))>0){
+        
+        now_time=current_now_time
         var arrival=document.getElementsByClassName("arrival")
         for(var i=0; i<arrival.length;i++){
-            var key= arrival[i].parentElement.getAttribute("key");
+            var key= arrival[i].parentElement.getAttribute("id");
             var eta= parseInt(arrival[i].textContent);
-           eta--
-         
-            if(eta>0){
-                arrival[i].textContent=eta
-               database.ref("/train/"+key).update({
-                eta:eta
-            })
-                 
-            }
-            else if(eta===0){//train arrives
+           eta--// countdown
+
+           if(eta===0){//train arrives
 
                 var frequency=arrival[i].previousSibling.previousSibling.textContent
-                var update=moment(arrival[i].previousSibling.textContent,"hh:mm")
+                var update=moment(arrival[i].previousSibling.textContent,"HH:mm")
                 var min_update=update.minute()+parseInt(frequency)
                 update.set("minute", min_update)//updated time value
-                var new_time=update.format("hh:mm")
+                var new_time=update.format("HH:mm")
                 arrival[i].textContent=frequency
                 arrival[i].previousSibling.textContent=new_time
                 database.ref("/train/"+key).update({
-                    
+
                     arrival:new_time,
                     eta:frequency
                 })
@@ -170,42 +258,119 @@ intervalId=setInterval(function(){
 
             }
 
+            else{
+                arrival[i].textContent=eta
+            }
+
         }
     }
+      
+    }    
 
 },1000)
 
 
 
+//detele train data
+
+function removing() {
+    console.log("hello")
+    var key = this.parentElement.parentElement.getAttribute("id");
+    var parent = document.getElementById(key)
+    document.getElementById("list").removeChild(parent)
+    database.ref("/train").child(key).remove()
+}
+//update data
+var train_update = document.getElementById("train_name_update")
+var frequency_update = document.getElementById("frequency_update")
+var destination_update = document.getElementById("destination_update")
+var arrival_update = document.getElementById("next_arrival_update")
+var update_button = document.getElementById("update_button")
+
+function update_data() {
+    var key = this.parentElement.parentElement.getAttribute("id");
+    var parent = this.parentElement
+
+    train_update.value = parent.nextSibling.textContent
+    destination_update.value = parent.nextSibling.nextSibling.textContent
+    frequency_update.value = parent.nextSibling.nextSibling.nextSibling.textContent
+    arrival_update.value = parent.nextSibling.nextSibling.nextSibling.nextSibling.textContent
+    update_button.setAttribute("key", key)
+}
+
+update_button.addEventListener("click", function () {
+    var train = train_update.value;
+    var destination = destination_update.value;
+    var frequency = frequency_update.value;
+    var arrival = arrival_update.value
+    var key = this.getAttribute("key")
+    database.ref("/train/" + key).update({
+
+        train: train,
+        destination: destination,
+        frequency: frequency,
+        arrival: arrival,
+    })
+
+})
+
+//modal area
+document.getElementById("Sign-in").addEventListener("click", User)
+document.getElementById("Log-in").addEventListener("click", User)
+
+function User() {
+    console.log("working")
+    var task = this.textContent.trim()
+    document.getElementById("Title").textContent = task
+    document.getElementById("task_button").textContent = task
+    document.getElementById("task_button").setAttribute("Task", task)
+}
 
 
-//Time
-
+var newish = firebase.auth.GoogleAuthProvider
 // // AUTHENTICATION
-//     var auth = firebase.auth()
+document.getElementById("task_button").addEventListener("click", Auth)
 
-//     var email_box=document.getElementById("email")
-//     var password_box=document.getElementById("password")
-
-//     document.getElementById("SignuP").addEventListener("click", function(){
-//         //check for valid email
-//         var email=email_box.value
-//         var password=password_box.value
-
-//         var user=auth.createUserWithEmailAndPassword(email,password)
-
-
-//     })
-
-//     firebase.auth().onAuthStateChanged(firebaseUser =>{
-
-//         if(firebaseUser){
-//             console.log(firebaseUser);
-//         } else{
-//             console.log("not logged in")
-//         }
-
-    
+function Auth() {
+    console.log("hi")
+    var task = this.textContent
+    console.log(task)
+    var auth = firebase.auth()
+    var email = document.getElementById("email")
+    var password = document.getElementById("password")
+    switch (task) {
+        case "Sign-in":
+            var user = auth.createUserWithEmailAndPassword(email.value, password.value)
+            break;
+        case "Log-In":
+            var user = auth.signInWithEmailAndPassword(email.value, password.value)
+            break;
+    }
+}
 
 
-    // })
+firebase.auth().onAuthStateChanged(firebaseUser => {
+
+    if (firebaseUser) {
+        document.getElementsByClassName("container")[0].style.display = "block"
+        document.getElementById("Log-out").style.display = "block"
+        document.getElementById("Log-in").style.display = "none"
+        document.getElementById("Sign-in").style.display = "none"
+    } else {
+        console.log("not logged in")
+    }
+})
+
+// logging out
+document.getElementById("Log-out").addEventListener("click", function () {
+    firebase.auth().signOut().then(function () {
+        console.log("bye")
+        document.getElementsByClassName("container")[0].style.display = "none"
+        document.getElementById("Log-out").style.display = "none"
+        document.getElementById("Log-in").style.display = "block"
+        document.getElementById("Sign-in").style.display = "block"
+    })
+
+
+})
+table_headers()
